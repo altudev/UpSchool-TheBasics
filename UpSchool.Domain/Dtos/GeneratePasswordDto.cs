64b835a7b1c42461721a649bf9b2307211d1bb15
AltuDev <altudev02@gmail.com>
2023-02-04 21:35:12 +0300

    First step of the UpStorage with PasswordGenerator.

---
+++ UpSchool.Domain/Dtos/GeneratePasswordDto.cs
@@ -0,0 +1,11 @@
+﻿namespace UpSchool.Domain.Dtos
+{
+    public class GeneratePasswordDto
+    {
+        public bool IncludeNumbers { get; set; } = true;
+        public bool IncludeLowercaseCharacters { get; set; } = true;
+        public bool IncludeUppercaseCharacters { get; set; }
+        public bool IncludeSpecialCharacters { get; set; }
+        public int Length { get; set; } = 6;
+    }
+}
---
+++ UpSchool.Domain/Utilities/PasswordGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using UpSchool.Domain.Dtos;
+
+namespace UpSchool.Domain.Utilities
+{
+    public class PasswordGenerator
+    {
+        private const string Numbers = "0123456789";
+        private const string LowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+        private const string UppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string SpecialCharacters = "!@#$%^&*()";
+
+        private readonly Random _random;
+        private readonly StringBuilder _passwordBuilder;
+        private readonly StringBuilder _charSetBuilder;
+
+        public PasswordGenerator()
+        {
+            _random = new Random();
+
+            _passwordBuilder = new StringBuilder();
+
+            _charSetBuilder = new StringBuilder();
+
+        }
+
+        public string Generate(GeneratePasswordDto generatePasswordDto)
+        {
+            _charSetBuilder.Clear();
+            _passwordBuilder.Clear();
+
+            if (generatePasswordDto.IncludeNumbers) _charSetBuilder.Append(Numbers);
+
+            if (generatePasswordDto.IncludeLowercaseCharacters) _charSetBuilder.Append(LowercaseCharacters);
+
+            if (generatePasswordDto.IncludeUppercaseCharacters) _charSetBuilder.Append(UppercaseCharacters);
+
+            if (generatePasswordDto.IncludeSpecialCharacters) _charSetBuilder.Append(SpecialCharacters);
+
+            var charSet = _charSetBuilder.ToString();
+
+            for (int i = 0; i < generatePasswordDto.Length; i++)
+            {
+                var randomIndex = _random.Next(charSet.Length);
+
+                _passwordBuilder.Append(charSet[randomIndex]);
+            }
+
+            return _passwordBuilder.ToString();
+        }
+
+    }
+}
