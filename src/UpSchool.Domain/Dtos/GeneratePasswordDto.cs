bef00931a1654fbd11f105a6a3f283cfbf2cb103
AltuDev <altudev02@gmail.com>
2023-02-21 22:08:31 +0300

    UpStorage Password Generator bottom card has been created. (21.02.2023)

--- src/UpSchool.Domain/Dtos/GeneratePasswordDto.cs
+++ src/UpSchool.Domain/Dtos/GeneratePasswordDto.cs
@@ -2,10 +2,19 @@
 {
     public class GeneratePasswordDto
     {
-        public bool IncludeNumbers { get; set; } = true;
-        public bool IncludeLowercaseCharacters { get; set; } = true;
+        public bool IncludeNumbers { get; set; }
+        public bool IncludeLowercaseCharacters { get; set; }
         public bool IncludeUppercaseCharacters { get; set; }
         public bool IncludeSpecialCharacters { get; set; }
-        public int Length { get; set; } = 6;
+        public int Length { get; set; }
+
+        public GeneratePasswordDto()
+        {
+            IncludeNumbers = true;
+
+            IncludeLowercaseCharacters = true;
+
+            Length = 25;
+        }
     }
 }
