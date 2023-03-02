aa63d53f5e4eaa14115591ba24768e502765e2a8
AltuDev <altudev02@gmail.com>
2023-03-02 22:24:25 +0300

    Routing in Blazor & SavedPasswordsCard Component were created.

---
+++ src/UpSchool.Wasm/Common/Utilities/CssUtilities.cs
@@ -0,0 +1,24 @@
+﻿namespace UpSchool.Wasm.Common.Utilities
+{
+    public static class CssUtilities
+    {
+        public static string GetCssColourClassForPasswords(int length)
+        {
+            switch (length)
+            {
+                case var value when (value >= 6 && value <= 12):
+                    return "bg-danger";
+
+                case var value when (value > 12 && value <= 19):
+                    return "bg-warning";
+
+                case var value when (value > 19 && value <= 40):
+                    return "bg-success";
+
+                default:
+                    throw new Exception("Beklenmedik bir password uzunluğu geldi.");
+                    break;
+            }
+        }
+    }
+}
