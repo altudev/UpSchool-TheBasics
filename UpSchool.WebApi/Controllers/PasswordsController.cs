68457d8f9eb1c4ea2c40402884fbc3734dc56737
AltuDev <altudev02@gmail.com>
2023-03-07 22:49:46 +0300

    Basics of the Web API.

---
+++ UpSchool.WebApi/Controllers/PasswordsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UpSchool.Domain.Dtos;
+using UpSchool.Domain.Utilities;
+
+namespace UpSchool.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordsController : ControllerBase
+    {
+        private readonly PasswordGenerator _passwordGenerator;
+        private readonly GeneratePasswordDto _generatePasswordDto;
+
+        public PasswordsController()
+        {
+            _passwordGenerator = new PasswordGenerator();
+
+            _generatePasswordDto = new GeneratePasswordDto
+            {
+                Length = 15,
+                IncludeSpecialCharacters = true,
+                IncludeUppercaseCharacters = true
+            };
+        }
+
+        [HttpGet]
+        public IActionResult GetPasswords()
+        {
+            List<string> passwords = new List<string>();
+
+            for (int i = 0; i < 9; i++)
+                passwords.Add(_passwordGenerator.Generate(_generatePasswordDto));
+
+            return Ok(passwords);
+        }
+    }
+}
--- src/UpSchool.Wasm/Program.cs
+++ src/UpSchool.Wasm/Program.cs
@@ -1,13 +1,11 @@
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using Blazored.LocalStorage;
 using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using UpSchool.Domain.Common;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using UpSchool.Domain.Services;
 using UpSchool.Wasm;
-using UpSchool.Wasm.Common.Utilities;
 using UpSchool.Wasm.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -17,7 +15,7 @@ var titanicFluteApiUrl = builder.Configuration.GetConnectionString("TitanicFlute
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7296/api/") });
 
 builder.Services.AddBlazoredToast();
 
