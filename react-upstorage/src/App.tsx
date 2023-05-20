7e4225d343a59404ba5956d60fad7c27ef6d8377
ALPER TUNGA <alper@altu.dev>
2023-05-20 21:17:32 +0300

    React project was created.

---
+++ react-upstorage/src/App.tsx
@@ -0,0 +1,43 @@
+import {useState} from 'react';
+import './App.css'
+import NavBar from './components/NavBar.tsx';
+
+function App() {
+
+    const [password, setPassword] = useState<string>("123456");
+
+    return (
+        <>
+            <NavBar />
+            <div className="container" style={{backgroundColor: "#C4DFDF"}}>
+                <div className="card-header is-justify-content-center">
+                    <h3 className="has-text-success is-size-2">Secure Password Generator</h3>
+                </div>
+                <div className="card">
+
+                    <div className="card-content">
+                        <div className="media">
+                            <div className="media-content">
+                                <p className="is-size-3">{password}</p>
+                            </div>
+                            <div className="media-right">
+                                <i className="is-size-3">📋</i>
+                                <i className="is-size-3">♻️</i>
+                            </div>
+                        </div>
+
+                        <div className="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                            <a href="#">#css</a> <a href="#">#responsive</a>
+                            <br/>
+                            <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ react-upstorage/src/components/NavBar.tsx
@@ -0,0 +1,45 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                </a>
+
+                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false"
+                   data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <a className="navbar-item">
+                        Home
+                    </a>
+
+                    <a className="navbar-item">
+                        Documentation
+                    </a>
+                </div>
+
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <a className="button is-primary">
+                                <strong>Sign up</strong>
+                            </a>
+                            <a className="button is-light">
+                                Log in
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default  NavBar;
